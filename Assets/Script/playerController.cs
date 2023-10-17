2870092f815310ad87aee9c07183c1eade53d7c1
TranQuyDat <datraumuong@gmail.com>
2023-10-17 17:32:01 +0700

    17/10/2023 new project start

---
+++ Assets/Script/playerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rb;
+    public float speed;
+    private Vector2 movement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetBool("isrun", (movement.x != 0 || movement.y != 0));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        flip();
+    }
+    public void flip()
+    {
+        float scalex = this.transform.localScale.x;
+        this.transform.localScale = new Vector2(
+            (movement.x==0 )? scalex :
+            (movement.x > 0)? Mathf.Abs(scalex): Mathf.Abs(scalex) * -1
+            , 1f);
+    }
+}
