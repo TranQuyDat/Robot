afb75b5340624ca99469f9c7621eae298506e110
TranQuyDat <datraumuong@gmail.com>
2023-10-23 01:17:08 +0700

    23/10/2023 update

---
+++ Assets/Script/Ai/ObstacleAvoidance.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAvoidance : SteeringBehaviour
+{
+    public float radius, agentColliderSize = 0.6f;
+    public float[] dangerResultTemp = null;
+    public bool showGizmos;
+
+    public override (float[] danger, float[] interest) GetSteering(float[] danger, float[] interest, AiData aiData)
+    {
+        foreach(Collider2D obs in aiData.obstacles)
+        {
+            Vector2 directionToObstacle = 
+                obs.ClosestPoint(transform.position) - (Vector2)transform.position;
+            float distanceToObstacle = directionToObstacle.magnitude;
+
+            float weight = distanceToObstacle < agentColliderSize ? 1 :
+                (radius - distanceToObstacle) / radius;
+
+            Vector2 directionToObstacleNormalized = directionToObstacle.normalized;
+            for (int i = 0; i < Directions.eightDirection.Count; i++)
+            {
+                float result = 
+                    Vector2.Dot(directionToObstacleNormalized, Directions.eightDirection[i]);
+                if(result > danger[i])
+                {
+                    danger[i] = result;
+                }
+            }
+        }
+        dangerResultTemp = danger;
+        return (danger, interest);
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (!showGizmos) return;
+
+        if (Application.isPlaying && dangerResultTemp != null)
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < dangerResultTemp.Length; i++)
+            {
+                Gizmos.DrawRay(transform.position,
+                    dangerResultTemp[i] * Directions.eightDirection[i]);
+            }
+        }
+
+        else
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
+static class Directions
+{
+    public static List<Vector2> eightDirection = new List<Vector2>()
+    {
+        new Vector2(0,1).normalized,
+        new Vector2(0,-1).normalized,
+        new Vector2(1,0).normalized,
+        new Vector2(-1,0).normalized,
+        new Vector2(1,1).normalized,
+        new Vector2(1,-1).normalized,
+        new Vector2(-1,1).normalized,
+        new Vector2(-1,-1).normalized,
+    };
+} 
---
+++ Assets/Script/Ai/SeekBehaviour.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SeekBehaviour : SteeringBehaviour
+{
+    public float targetThreshold;
+    public bool showGizmos = true;
+    bool reachedLastTarget = true;
+    public Vector2 targetPositionCached;
+    public float[] interestsTemp;
+    public override (float[] danger, float[] interest) GetSteering(float[] danger, float[] interest, AiData aiData)
+    {
+        if (reachedLastTarget == true)
+        {
+            if (aiData.targets == null || aiData.targets.Count <= 0)
+            {
+                aiData.currentTarget = null;
+                return (danger, interest);
+            }
+            else
+            {
+                reachedLastTarget = false;
+                aiData.currentTarget = aiData.targets.OrderBy(target =>
+                Vector2.Distance(target.position, transform.position)).FirstOrDefault();
+            }
+        }
+
+        if(aiData.currentTarget != null && aiData.targets != null 
+            && aiData.targets.Contains(aiData.currentTarget))
+        {
+            targetPositionCached = aiData.currentTarget.position; 
+        }
+
+        if(Vector2.Distance(targetPositionCached,transform.position) < targetThreshold)
+        {
+            reachedLastTarget = true;
+            aiData.currentTarget = null;
+            return (danger, interest);
+        }
+
+        Vector2 directionTotarget = (targetPositionCached - (Vector2)transform.position).normalized;
+        for(int i = 0; i < interest.Length; i++)
+        {
+            float result = Vector2.Dot(directionTotarget, Directions.eightDirection[i]);
+            if(result > 0)
+            {
+                float ValueToPutIn = result;
+                if(ValueToPutIn > interest[i])
+                {
+                    interest[i] = ValueToPutIn;
+                }
+            }
+        }
+        interestsTemp = interest;
+        return (danger, interest);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos) return;
+        Gizmos.DrawSphere(targetPositionCached, 0.2f);
+        if(Application.isPlaying && interestsTemp != null)
+        {
+            Gizmos.color = Color.green;
+            for(int i = 0; i < interestsTemp.Length; i++)
+            {
+                Gizmos.DrawRay(transform.position, Directions.eightDirection[i]*interestsTemp[i]);
+            }
+            if (reachedLastTarget == false)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(transform.position, 0.1f);
+            }
+        }
+    }
+}
